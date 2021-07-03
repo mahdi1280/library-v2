74d0f05531a79e11ae41b1b51d4d3092ebd95fe0
pylie <pylie@gmail.com>
2021-07-03 15:05:32 +0430

    book serivce update

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -3,7 +3,11 @@ package com.example.library.service;
 import com.example.library.Repository.BookRepo;
 import com.example.library.model.dto.BookDto;
 import com.example.library.model.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -12,15 +16,24 @@ public class BookService {
     public BookService(BookRepo bookRepo) {
         this.bookRepo = bookRepo;
     }
-    private Book add(BookDto bookDto){
+    public Book add(BookDto bookDto){
         Book book = new Book();
         book.setName(bookDto.getName());
         book.getCategories().add(bookDto.getCategory());
-        book.getUsers().add(bookDto.getUser());
         return bookRepo.save(book);
     }
-//    private Book update(Long id,BookDto bookDto){
-//        bookRepo.findById(id);
-//
-//    }
+    public Book update(Long id,BookDto bookDto){
+       Book book=bookRepo.findById(id).orElse(null);
+       if(book!= null){
+           book.setName(bookDto.getName());
+           book.getCategories().clear();
+           book.getCategories().add(bookDto.getCategory());
+       }
+       return null;
+    }
+    public Page<Book>  show(Pageable pageable){
+       Page<Book> books= bookRepo.findAll(pageable);
+       return books;
+    }
+    
 }
