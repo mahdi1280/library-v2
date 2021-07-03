c7f3ae48c8309e172cd61cee0244e32fe9a55b99
mohammad-ebrahini <mohammadebe@gmail.com>
2021-07-03 16:15:53 +0430

    book-controller

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -3,6 +3,8 @@ package com.example.library.controller;
 import com.example.library.model.dto.BookDto;
 import com.example.library.model.entity.Book;
 import com.example.library.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +15,20 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
     @PostMapping("/add")
-    public Book add(@RequestBody BookDto bookDto){
+    public Book add(@RequestBody BookDto bookDto) {
         return bookService.add(bookDto);
+    }
+
+    @PostMapping("/update/{id}")
+    public Book update(@RequestBody BookDto bookDto, @PathVariable Long id) {
+
+        return bookService.update(id, bookDto);
+    }
+    @GetMapping("/show")
+    public Page<Book> show(Pageable pageable){
+        return bookService.show(pageable);
+
     }
 }
--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -8,34 +8,37 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class BookService {
     private final BookRepo bookRepo;
     private final CategoryRepo categoryRepo;
-    public BookService(BookRepo bookRepo,CategoryRepo categoryRepo) {
+
+    public BookService(BookRepo bookRepo, CategoryRepo categoryRepo) {
         this.bookRepo = bookRepo;
         this.categoryRepo = categoryRepo;
     }
-    public Book add(BookDto bookDto){
+
+    public Book add(BookDto bookDto) {
         Book book = new Book();
         book.setName(bookDto.getName());
-        System.out.println(categoryRepo.findById(1L).orElse(null));
-        book.getCategories().add(categoryRepo.findById(1L).get());
+        book.getCategories().add(categoryRepo.findById(1L).orElse(null));
         return bookRepo.save(book);
     }
-    public Book update(Long id,BookDto bookDto){
-       Book book=bookRepo.findById(id).orElse(null);
-       if(book!= null){
-           book.setName(bookDto.getName());
-           book.getCategories().clear();
-           book.getCategories().add(categoryRepo.getById(bookDto.getCategoryId()));
-       }
-       return null;
+
+    public Book update(Long id, BookDto bookDto) {
+        Book book = bookRepo.findById(id).orElse(null);
+        if (book != null) {
+            book.setName(bookDto.getName());
+            book.getCategories().clear();
+            book.getCategories().add(categoryRepo.getById(bookDto.getCategoryId()));
+            return bookRepo.save(book);
+        }
+        return null;
+
     }
-    public Page<Book>  show(Pageable pageable){
-       Page<Book> books= bookRepo.findAll(pageable);
-       return books;
+
+    public Page<Book> show(Pageable pageable) {
+        Page<Book> books = bookRepo.findAll(pageable);
+        return books;
     }
 }
