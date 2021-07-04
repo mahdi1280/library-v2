0f71fc3d4ebf380508f17f4dbe3aecea77e1baf0
mohammad-ebrahini <mohammadebe@gmail.com>
2021-07-04 09:29:17 +0430

    created-reserv-controller

--- src/main/java/com/example/library/controller/ReservController.java
+++ src/main/java/com/example/library/controller/ReservController.java
@@ -1,4 +1,34 @@
 package com.example.library.controller;
 
+import com.example.library.model.dto.ReservDto;
+import com.example.library.model.entity.Reserv;
+import com.example.library.service.ReservService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reserv")
 public class ReservController {
+
+    private final ReservService reservService;
+
+    public ReservController(ReservService reservService) {
+        this.reservService = reservService;
+    }
+
+    @PostMapping("/add")
+    public Reserv add(@RequestBody ReservDto reservDto) {
+        return reservService.add(reservDto);
+    }
+
+    @GetMapping("/update/{id}")
+    public Reserv update(@PathVariable Long id, ReservDto reservDto) {
+        return null;
+    }
+
+    @PostMapping("/show")
+    public Page<Reserv> show(Pageable pageable) {
+        return reservService.show(pageable);
+    }
 }
