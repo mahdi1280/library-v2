54d443ccbcba202529b3470feebb090e364775c7
mohammad-ebrahini <mohammadebe@gmail.com>
2021-07-04 09:17:14 +0430

    user-controller-update

--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.library.controller;
 
+import com.example.library.model.dto.UserDto;
+import com.example.library.model.entity.User;
 import com.example.library.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -12,5 +15,23 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/add")
+    public User addUser(@RequestBody UserDto userDto) {
+        return userService.add(userDto);
+    }
+
+    @GetMapping("/update/{id}")
+    public User update(@RequestBody UserDto userDto, @PathVariable Long id) {
+
+        return userService.update(id, userDto);
+
+    }
+
+    @PostMapping("/show")
+
+    public Page<User> show(Pageable pageable) {
+        return userService.show(pageable);
     }
 }
