4a9ac23e95e4201c0b9e906d09006351f3242664
pylie <pylie@gmail.com>
2021-07-04 09:14:43 +0430

    CategoryService and ReservService

--- src/main/java/com/example/library/service/ReservService.java
+++ src/main/java/com/example/library/service/ReservService.java
@@ -3,6 +3,8 @@ package com.example.library.service;
 import com.example.library.Repository.ReservRepo;
 import com.example.library.model.dto.ReservDto;
 import com.example.library.model.entity.Reserv;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,4 +20,9 @@ public class ReservService {
         Reserv reserv = new Reserv();
         return reserv;
     }
+
+    public Page<Reserv> show(Pageable pageable){
+        return reservRepo.findAll(pageable);
+    }
+
 }
