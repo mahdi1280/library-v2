76fd50ba24f7c471699025e8d239e3f9cfa8dfff
mohammad-ebrahini <mohammadebe@gmail.com>
2021-07-04 09:02:52 +0430

    make-user-contoller

--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.example.library.controller;
 
+import com.example.library.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
